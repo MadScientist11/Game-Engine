40c2e9ce677909eb01065cb78758c3d62191783e
ashirogimuta <quizandpuzzle@gmail.com>
2023-07-18 20:16:43 +0300

    Transform

---
+++ GameEngine/Core/Component.cs
@@ -0,0 +1,20 @@
+﻿namespace GameEngine.Core;
+
+public abstract class Component
+{
+    public GameObject GameObject { get; set; }
+
+
+   
+
+    public void AddComponent<T>(T component) where T : Component
+    {
+        GameObject.Components.Add(component);
+    }
+
+    public bool HasComponent<T>(out T component) where T : Component
+    {
+        component = (T)GameObject.Components.Find(c => c is T)!;
+        return component != null;
+    }
+}
---
+++ GameEngine/Core/Transform.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+namespace GameEngine.Core;
+
+public class Transform : Component
+{
+    public Vector3 Position { get; set; }
+    public Vector3 Scale { get; set; }
+    public Quaternion Rotation { get; set; }
+
+    public Matrix4x4 TRS => Matrix4x4.CreateScale(Scale) *
+                            Matrix4x4.CreateFromQuaternion(Rotation) *
+                            Matrix4x4.CreateTranslation(Position);
+
+ 
+
+   
+}
